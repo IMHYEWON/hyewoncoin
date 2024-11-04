196a546a95147c8f2523f81f433e7908b6efbdbe
IMHYEWON <hyewon1118@gmail.com>
2024-11-05 00:23:06 +0900

    4.1 Our First Block

---
+++ 4. blockchain/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+type block struct {
+	data     string
+	hash     string
+	prevHash string
+}
+
+// Hash function : one way function
+// "abc" -> hash("abc") -> "dasdwd342dskd"
+
+func main() {
+	genesisBlock := block{"Genesis Block", "", ""}
+
+	// Sum256 function takes a byte slice and returns a byte slice
+	hash := sha256.Sum256([]byte(genesisBlock.data + genesisBlock.prevHash))
+	fmt.Println(hash)
+	fmt.Printf("%x", hash) // %x -> hexa decimal
+	hexHash := fmt.Sprintf("%x", hash)
+	genesisBlock.hash = hexHash // get Hash from "Genesis Block" and assign it to hash
+	fmt.Println(genesisBlock)
+
+}
