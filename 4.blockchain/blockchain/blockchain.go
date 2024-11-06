2b3f9c12f2965a3550ebf7b3e7cf6968aec411ee
IMHYEWON <hyewon1118@gmail.com>
2024-11-06 22:55:27 +0900

    4.4 블록체인 초기화 & 생성 동기처리

--- 4.blockchain/blockchain/blockchain.go
+++ 4.blockchain/blockchain/blockchain.go
@@ -1,5 +1,7 @@
 package blockchain
 
+import "sync"
+
 type block struct {
 	data     string
 	hash     string
@@ -13,13 +15,19 @@ type blockchain struct {
 // blockchain.go 내부에서만 이 변수에 접근 가능
 var b *blockchain
 
+// sync.Once : 한번만 실행되는 코드를 실행하기 위한 구조체
+var once sync.Once
+
 // 한번만 실행되는 코드
 // 이 메소드로 블록체인 생성을 제어
 func GetBlockChain() *blockchain {
 	// nil : 아무것도 없음을 나타내는 특별한 값
 	// b가 nil이면 새로운 blockchain을 생성하여 반환
 	if b == nil {
-		b = &blockchain{}
+		// 블록체인 인스턴스 생성이 단 한번만 실행되도록 보장
+		once.Do(func() {
+			b = &blockchain{}
+		})
 	}
 
 	// 블록체인 인스턴스 반인
