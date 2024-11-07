a5f294385ecd6d0fa55ff1108facc2130e5b9ab9
IMHYEWON <hyewon1118@gmail.com>
2024-11-07 23:54:11 +0900

    4.4 블록을 생성하는 함수를 만들어 블록체인 생성시 호출

--- 4.blockchain/blockchain/blockchain.go
+++ 4.blockchain/blockchain/blockchain.go
@@ -1,6 +1,10 @@
 package blockchain
 
-import "sync"
+import (
+	"crypto/sha256"
+	"fmt"
+	"sync"
+)
 
 type block struct {
 	data     string
@@ -9,7 +13,7 @@ type block struct {
 }
 
 type blockchain struct {
-	blocks []block
+	blocks []*block
 }
 
 // blockchain.go 내부에서만 이 변수에 접근 가능
@@ -17,6 +21,29 @@ var b *blockchain
 
 // sync.Once : 한번만 실행되는 코드를 실행하기 위한 구조체
 var once sync.Once
+
+// 블록의 해시값을 계산해서 리턴
+func (bl *block) getCalculateHash() string {
+	hash := sha256.Sum256([]byte(bl.data + bl.prevHash))
+	bl.hash = fmt.Sprintf("%x", hash)
+	return bl.hash
+}
+
+// 마지막 블록의 해시값을 리턴
+func getLastHash() string {
+	totalBlocks := len(GetBlockChain().blocks)
+	if totalBlocks == 0 {
+		return ""
+	}
+	return GetBlockChain().blocks[totalBlocks-1].hash
+}
+
+// 새로운 블록을 생성
+func createBlock(data string) *block {
+	newBlock := block{data, "", getLastHash()}
+	newBlock.hash = newBlock.getCalculateHash()
+	return &newBlock
+}
 
 // 한번만 실행되는 코드
 // 이 메소드로 블록체인 생성을 제어
@@ -27,6 +54,7 @@ func GetBlockChain() *blockchain {
 		// 블록체인 인스턴스 생성이 단 한번만 실행되도록 보장
 		once.Do(func() {
 			b = &blockchain{}
+			b.blocks = append(b.blocks, createBlock("Genesis Block"))
 		})
 	}
 
