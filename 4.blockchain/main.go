cf74d87b8e9f9a034f395d3e4d628c8923f838ce
IMHYEWON <hyewon1118@gmail.com>
2024-11-09 23:19:29 +0900

    5.0 웹서버 셋업

--- 4.blockchain/main.go
+++ 4.blockchain/main.go
@@ -2,22 +2,24 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/IMHYEWON/hyewoncoin/4.blockchain/blockchain"
+	"log"
+	"net/http"
 )
 
+const port string = ":4000"
+
+// ResponseWriter : 응답을 작성하는 인터페이스
+// Request : 클라이언트의 요청을 나타내는 구조체 (*포인터)
+func home(rw http.ResponseWriter, r *http.Request) {
+	// Fprint : 문자열을 콘솔이 아닌 writer에 출력
+	fmt.Fprint(rw, "Hello from home")
+}
+
 func main() {
-	// GetBlockChain() 함수를 호출하여 블록체인 인스턴스를 생성
-	// 이미 Genesis Block이 생성되어 있음
-	chain := blockchain.GetBlockChain()
+	// Node.js의 라우터와 비슷한 기능
+	http.HandleFunc("/", home)
+	fmt.Printf("Listening on http://localhost%s\n", port)
 
-	chain.AddBlock("Second Block")
-	chain.AddBlock("Third Block")
-	chain.AddBlock("Fourth Block")
-
-	for _, block := range chain.AllBlocks() {
-		fmt.Println("Data : ", block.Data)
-		fmt.Println("Hash : ", block.Hash)
-		fmt.Println("PrevHash : ", block.PrevHash)
-	}
+	// log.Fatal : 프로그램 에러 발생시 프로그램 종료 후 메시지 출력
+	log.Fatal(http.ListenAndServe(port, nil))
 }
