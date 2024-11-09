4601a487d6723a0aca94b27fd6ffe23a32ae2a4c
IMHYEWON <hyewon1118@gmail.com>
2024-11-10 00:06:38 +0900

    5.3 Using Partail

--- 4.blockchain/main.go
+++ 4.blockchain/main.go
@@ -10,6 +10,9 @@ import (
 )
 
 const port string = ":4000"
+const templateDir string = "templates/"
+
+var templates *template.Template
 
 type homeData struct {
 	PageTitle string
@@ -19,25 +22,16 @@ type homeData struct {
 // ResponseWriter : 응답을 작성하는 인터페이스
 // Request : 클라이언트의 요청을 나타내는 구조체 (*포인터)
 func home(rw http.ResponseWriter, r *http.Request) {
-	// Fprint : 문자열을 콘솔이 아닌 writer에 출력
-	// fmt.Fprint(rw, "Hello from home")
-	//tmpl, err := template.ParseFiles("templates/home.html")
-
-	// Go에는 Exception이나 Error가 없으므로, 직접 처리해야함
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// Must : 템플릿을 파싱하고 에러가 있으면 프로그램을 종료 (에러핸들링을 하지 않아도 됨핸
-	tmpl := template.Must(template.ParseFiles("templates/home.html"))
-
 	blockchain := blockchain.GetBlockChain()
 	data := homeData{PageTitle: "Blockchain Home Page", Blocks: blockchain.AllBlocks()}
 	// Execute : 템플릿을 렌더링하여 출력
-	tmpl.Execute(rw, data)
+	templates.ExecuteTemplate(rw, "home", data)
 }
 
 func main() {
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
+
 	// Node.js의 라우터와 비슷한 기능
 	http.HandleFunc("/", home)
 	fmt.Printf("Listening on http://localhost%s\n", port)
