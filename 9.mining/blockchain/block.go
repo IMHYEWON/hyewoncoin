98b4c39b6cb49154d6f265a1ae25cc6a9974369c
IMHYEWON <hyewon1118@gmail.com>
2024-11-30 23:57:04 +0900

    ✨ 9.2 블록 채굴

--- 9.mining/blockchain/block.go
+++ 9.mining/blockchain/block.go
@@ -4,6 +4,7 @@ import (
 	"crypto/sha256"
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/IMHYEWON/hyewoncoin/9.mining/db"
 	"github.com/IMHYEWON/hyewoncoin/9.mining/utils"
@@ -43,17 +44,37 @@ func FindBlock(hash string) (*Block, error) {
 	return block, nil
 }
 
+func (b *Block) mine() {
+	target := strings.Repeat("0", difficulty)
+
+	for {
+		blockAsString := fmt.Sprint(b)
+
+		hash := fmt.Sprintf("%x", sha256.Sum256([]byte(blockAsString)))
+		fmt.Printf("Block as String: %s\nHash: %s\nNonce: %d\n\n", blockAsString, hash, b.Nonce)
+
+		// hash값이 target값으로 시작하는지 확인
+		if strings.HasPrefix(hash, target) {
+			b.Hash = hash
+			break
+		} else {
+			b.Nonce++
+		}
+	}
+}
+
 func createBlock(data string, prevHash string, height int) *Block {
 	block := &Block{
-		Data:     data,
-		Hash:     "",
-		PrevHash: prevHash,
-		Height:   height,
+		Data:       data,
+		Hash:       "",
+		PrevHash:   prevHash,
+		Height:     height,
+		Difficulty: difficulty,
+		Nonce:      0,
 	}
 
-	payload := block.Data + block.PrevHash + string(block.Height)
-	block.Hash = fmt.Sprintf("%x", sha256.Sum256([]byte(payload)))
-
+	// 블록을 마이닝하고 저장
+	block.mine()
 	block.persist()
 
 	return block
--- 9.mining/main.go
+++ 9.mining/main.go
@@ -1,32 +1,9 @@
 package main
 
 import (
-	"crypto/sha256"
-	"fmt"
-	"strings"
-	"time"
+	"github.com/IMHYEWON/hyewoncoin/9.mining/cli"
 )
 
 func main() {
-	difficulty := 6
-	target := strings.Repeat("0", difficulty)
-	nonce := 1 // nonce란 ? : 블록체인에서 사용되는 숫자로, 블록을 찾기 위해 계속 변경되는 숫자
-
-	start := time.Now()
-
-	for {
-		hash := fmt.Sprintf("%x", sha256.Sum256([]byte("hello"+fmt.Sprint(nonce))))
-		// fmt.Printf("%x\n", hash)
-
-		// hash값이 target값으로 시작하는지 확인
-		if strings.HasPrefix(hash, target) {
-			fmt.Printf("Found hash: %s (Target: %s)\n nonce: %d\n\n", hash, target, nonce)
-			break
-		} else {
-			nonce++
-		}
-	}
-
-	elapsed := time.Since(start)
-	fmt.Printf("Elapsed Time: %s\n", elapsed)
+	cli.Start()
 }
